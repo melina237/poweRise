dc3caecf4706499370e771d526d568cfc847197e
githubt@tokyuez.net <github@tokyuez.net>
2024-01-27 19:25:56 +0100

    addded alarm

--- powerRise/app/src/main/java/com/example/powerise/MainActivity.java
+++ powerRise/app/src/main/java/com/example/powerise/MainActivity.java
@@ -1,12 +1,16 @@
 // MainActivity.java
 package com.example.powerise;
 
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     private SensorActivity lightSensorActivity;
@@ -17,6 +21,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         lightSensorActivity = new SensorActivity(this);
+
+        // Schedule the alarms when the app starts
+        scheduleAlarm();
+    }
+
+    private void scheduleAlarm() {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        // Weekdays Alarm
+        Intent weekdayIntent = new Intent(this, WeekdayReceiver.class);
+        PendingIntent weekdayPendingIntent = PendingIntent.getBroadcast(this, 0, weekdayIntent, 0);
+
+        // Weekends Alarm
+        Intent weekendIntent = new Intent(this, WeekendReceiver.class);
+        PendingIntent weekendPendingIntent = PendingIntent.getBroadcast(this, 1, weekendIntent, 0);
+
+        // Set the alarms
+        setRepeatingAlarm(alarmManager, weekdayPendingIntent, Calendar.MONDAY, 6);
+        setRepeatingAlarm(alarmManager, weekendPendingIntent, Calendar.SATURDAY, 8);
+    }
+
+    private void setRepeatingAlarm(AlarmManager alarmManager, PendingIntent pendingIntent, int dayOfWeek, int hourOfDay) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_WEEK, dayOfWeek);
+        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+        // Repeat weekly
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY * 7, pendingIntent);
     }
 
     @Override
@@ -25,16 +60,15 @@ public class MainActivity extends AppCompatActivity {
         // Call the onResume method of SensorActivity
         lightSensorActivity.onResume();
     }
+
     public void startSoundRecorder(View view) {
         Intent intent = new Intent(this, SoundRecorder.class);
         startActivity(intent);
-
     }
 
     public void startAlarm(View view) {
         Intent intent = new Intent(this, Alarm.class);
         startActivity(intent);
-
     }
 
     @Override
@@ -44,4 +78,3 @@ public class MainActivity extends AppCompatActivity {
         lightSensorActivity.onPause();
     }
 }
-
